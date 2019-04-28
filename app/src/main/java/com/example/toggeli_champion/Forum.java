7693735b6e57576f2f8dc9a3ead63a9fb86bb00b
Losh94 <43777331+Losh94@users.noreply.github.com>
2019-04-28 12:45:12 +0200

    auswahl

--- app/src/main/java/com/example/toggeli_champion/Forum.java
+++ app/src/main/java/com/example/toggeli_champion/Forum.java
@@ -1,11 +1,15 @@
 package com.example.toggeli_champion;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class Forum extends Activity {
+public class Forum extends Activity implements NavigationView.OnNavigationItemSelectedListener {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,39 @@ public class Forum extends Activity {
     private void setSupportActionBar(Toolbar toolbar) {
 
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.Start) {
+
+        } else if (id == R.id.newGame) {
+            Intent startNewGame = new Intent(Forum.this, NewGame.class);
+            startActivity(startNewGame);
+
+
+        } else if (id == R.id.Rangliste) {
+            Intent startRangliste = new Intent(Forum.this, Rangliste.class);
+            startActivity(startRangliste);
+
+
+        } else if (id == R.id.Forum) {
+            Intent startNewGame = new Intent(Forum.this, Forum.class);
+            startActivity(startNewGame);
+
+        } else if(id == R.id.TippsundTricks){
+            Intent startNewGame = new Intent(Forum.this, TippsundTricks.class);
+            startActivity(startNewGame);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 
 }
